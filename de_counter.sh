f3b50cbaafc7d2ec2c6d71bb049b82b6b611d8db
root <root@LAPTOP-J8R031G1.localdomain>
2022-03-14 18:46:05 +0100

    Modified the script with argument check

--- de_counter.sh
+++ de_counter.sh
@@ -3,13 +3,19 @@
 # This script counts the amount of accurancies of an article "de" in a file
 
 # First command line argument
-TEXT=$1
+FILE=$1
 
-# Check if the file is empty
-if [ -z "$TEXT" ]
+# Check if the argument contains a value
+if [ -z "$FILE" ]
 then
-    echo "Error: the file is empty"
+    echo "Error: please insert a file you want to work with"
     exit
+else
+    if [ -f "$FILE" ] # Check if the inserted file exists
+    then
+        less $FILE | egrep -wi '[a-z][a-z]' | grep -wio 'de' | wc -l
+    else
+        echo "Error: $FILE is not found in current directory"
+        exit
+    fi
 fi
-
-less $TEXT | egrep -wi '[a-z][a-z]' | grep --color -wio 'de' | wc -l
